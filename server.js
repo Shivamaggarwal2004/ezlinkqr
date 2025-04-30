88893098e7124e22f69143a0e89eda0b73e199b6
Shivamaggarwal2004 <rangesaini1011@gmail.com>
2025-05-01 00:29:43 +0530

    server.js

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT  || 5700;
 
+app.use(express.urlencoded({extended:false}));
+app.use(express.json())
+
+
 
 app.use(express.static(path.join(__dirname, "public")));
 
